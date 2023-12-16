3b2812418d20a70b3f72d02cc977ff48b91ca679
Pratham427 <109758178+Pratham427@users.noreply.github.com>
2023-12-16 15:13:27 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+class Student {
+    int rollno[] = new int[5];
+    String sname[] = new String[5], add[] = new String[5], mob[] = new String[5];
+    static int i = 0;
+    Scanner sc = new Scanner(System.in);
+
+    public void inputData() {
+        System.out.println("Enter Roll No: ");
+        rollno[i] = sc.nextInt();
+        System.out.println("Enter Name: ");
+        sname[i] = sc.next();
+        System.out.println("Enter Mobile: ");
+        mob[i] = sc.next();
+        System.out.println("Enter Address: ");
+        add[i] = sc.next();
+        ++i;
+    }
+
+    public void showInfo() {
+        System.out.println("RollNo\tSname\tMobile\tAddress");
+        System.out.println("-------------------------------------------");
+        for (int k = 0; k < i; ++k) {
+            System.out.println(rollno[k] + "\t" + sname[k] + "\t" + mob[k] + "\t" + add[k]);
+        }
+    }
+
+    public void updateInfo() {
+        System.out.println("Enter roll no: ");
+        int rno = sc.nextInt();
+        for (int k = 0; k < i; ++k) {
+            if (rno == rollno[k]) {
+                System.out.println("Your data is: ");
+                System.out.println("1. Name: " + sname[k]);
+                System.out.println("2. Mobile: " + mob[k]);
+                System.out.println("3. Address: " + add[k]);
+                System.out.println("Make your choice to update data: ");
+                int ch = sc.nextInt();
+                switch (ch) {
+                    case 1:
+                        System.out.println("Enter new name: ");
+                        sname[k] = sc.next();
+                        break;
+                    case 2:
+                        System.out.println("Enter new mobile: ");
+                        mob[k] = sc.next();
+                        break;
+                    case 3:
+                        System.out.println("Enter new address: ");
+                        add[k] = sc.next();
+                        break;
+                    default:
+                        System.out.println("Invalid Input");
+                        break;
+                }
+                System.out.println("Your data is updated");
+            }
+        }
+    }
+
+    public void delInfo() {
+        System.out.println("Enter roll no: ");
+        int rno = sc.nextInt();
+        for (int k = 0; k < i; ++k) {
+            if (rno == rollno[k]) {
+                int loc = k;
+                for (int j = loc; j < i - 1; ++j) {
+                    rollno[j] = rollno[j + 1];
+                    sname[j] = sname[j + 1];
+                    add[j] = add[j + 1];
+                    mob[j] = mob[j + 1];
+                }
+                System.out.println("Data deleted!!");
+                --i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Student m = new Student();
+        Scanner sc1 = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n1. Enter New Info");
+            System.out.println("2. Show Info");
+            System.out.println("3. Update Info");
+            System.out.println("4. Delete Info");
+            System.out.println("5. Exit");
+            System.out.println("Select an option: ");
+            int ch = sc1.nextInt();
+            switch (ch) {
+                case 1:
+                    m.inputData();
+                    break;
+                case 2:
+                    m.showInfo();
+                    break;
+                case 3:
+                    m.updateInfo();
+                    break;
+                case 4:
+                    m.delInfo();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
